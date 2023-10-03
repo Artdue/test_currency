4cee6f2bbd59ffc55f53ff1fb30e74056aa9fa87
Artdue <arttdude@gmail.com>
2023-10-03 15:10:01 +0300

    done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # test_currency
+
+# 1 run - npm i
+
+# 2 run - npm run dev
--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -7,21 +7,22 @@ import {
   Tooltip,
   Legend,
 } from 'recharts';
+import { ChartProps } from '../types/types';
 
-export function Chart({ data }) {
-  const currencyKeys = Object.keys(data[0][1]);
+export function Chart({ currData }: ChartProps) {
+  const currencyKeys = Object.keys(currData[0][1]);
 
   const minValue = Math.min(
-    ...data.map((entry: number) => Math.min(...Object.values(entry[1])))
+    ...currData.map((entry) => Math.min(...Object.values(entry[1])))
   );
   const maxValue = Math.max(
-    ...data.map((entry: number) => Math.max(...Object.values(entry[1])))
+    ...currData.map((entry) => Math.max(...Object.values(entry[1])))
   );
 
-  const dates = data.map((item: [string, unknown][]) => item[0]);
+  const dates = currData.map((item) => item[0]);
 
   return (
-    <LineChart width={800} height={400} data={data}>
+    <LineChart width={800} height={400} data={currData}>
       <CartesianGrid strokeDasharray='3 3' />
       <XAxis
         dataKey='date'
